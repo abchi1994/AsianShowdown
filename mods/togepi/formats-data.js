7534c68e146dfd5783274bc199cc94779e324b51
abchi1994 <abchi1994@gmail.com>
2016-08-17 18:00:36 -0700

    Pokemon Showdown Clone

---
+++ mods/togepi/formats-data.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.BattleFormatsData = {
+	
+	togepi: {
+			randomBattleMoves: ["metronome"],
+			eventPokemon: [
+				{"generation": 3, "level": 20, "gender": "F", "abilities":["serenegrace"], "moves":["metronome", "charm", "sweetkiss", "yawn"]},
+				{"generation": 3, "level": 25, "moves":["triattack", "followme", "ancientpower", "helpinghand"]},
+			],
+			tier: "LC",
+		},
+};
---
+++ mods/togepi/pokedex.js
@@ -0,0 +1,17 @@
+'use strict';
+
+exports.BattlePokedex = {
+	togepi: {
+			num: 175,
+			species: "Togepi",
+			types: ["Fairy"],
+			genderRatio: {M: 0.875, F: 0.125},
+			baseStats: {hp: 35, atk: 20, def: 65, spa: 40, spd: 65, spe: 20},
+			abilities: {0: "Hustle", 1: "Serene Grace", H: "Super Luck"},
+			heightm: 0.3,
+			weightkg: 1.5,
+			color: "White",
+			evos: ["togetic"],
+			eggGroups: ["Undiscovered"],
+		},
+};
